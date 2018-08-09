e61ba1d8db469ef34c387b18fff5ae90060a551e
Shelby Juno <shelbyjuno@gmail.com>
2018-08-08 20:54:46 -0500

    Timer
    -Added a UI timer
    - hh:mm:ss

---
+++ TestProject/Assets/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private Text timerText;
+    private float timer;
+    private float hours, minutes, seconds;
+
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+        hours = Mathf.Floor((timer % 216000) / 3600);
+        minutes = Mathf.Floor((timer % 3600) / 60);
+        seconds = (timer % 60);
+        timerText.text = hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
