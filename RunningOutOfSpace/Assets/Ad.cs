3ad4c312fa72a2392ba76746ad3901bf625a8767
Shelby Juno <shelbyjuno@gmail.com>
2018-08-10 18:58:51 -0500

    UI test
    UI implentation

---
+++ RunningOutOfSpace/Assets/Ad.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Ad : MonoBehaviour, IDragHandler, IEndDragHandler {
+
+    [System.Serializable]
+    public enum AdType
+    {
+        Closable,
+        Test,
+        Test2,
+        Test3
+    }
+
+    public AdType adType;
+
+    Vector3 tempMousePos;
+    void Start()
+    {
+        //if (adType == AdType.Closable)
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            tempMousePos = Input.mousePosition;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //
+    }
+
+    public void CloseAd()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ RunningOutOfSpace/Assets/AdSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdSpawner : MonoBehaviour {
+
+    public GameObject[] ads;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnAd", 0, 1f);
+    }
+
+    void SpawnAd()
+    {
+        int randomAd = Random.Range(0, ads.Length);
+
+        float xPos = Random.Range(0, Screen.width);
+        float yPos = Random.Range(0, Screen.height);
+
+        Vector3 spawnPoint = new Vector3(xPos, yPos, 0);
+
+        Instantiate(ads[randomAd], spawnPoint, Quaternion.identity, transform);
+    }
+}
