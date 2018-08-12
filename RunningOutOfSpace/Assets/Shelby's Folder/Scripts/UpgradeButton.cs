0a114202c2ad6ab1c5d1f6329a0ebc17be94696c
Shelby Juno <shelbyjuno@gmail.com>
2018-08-12 00:37:11 -0500

    upgrade store
    -Added UpgradeUI
    -Added buttons
    -bug fixes
    -Better code
    -lol

---
+++ RunningOutOfSpace/Assets/Shelby's Folder/Scripts/UpgradeButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UpgradeButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Sprite[] upgradeSprites;
+    //1 = Normal
+    //2 = Hover
+    //3 = Unavailable
+
+    public bool upgradeUnavailable = false;
+
+    void Update()
+    {
+        if (upgradeUnavailable)
+        {
+            GetComponent<Image>().sprite = upgradeSprites[2];
+            GetComponent<Button>().enabled = false;  
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!upgradeUnavailable)
+            GetComponent<Image>().sprite = upgradeSprites[0];
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {       
+        if (!upgradeUnavailable)
+            GetComponent<Image>().sprite = upgradeSprites[1];
+    }
+}
---
+++ RunningOutOfSpace/Assets/Shelby's Folder/Scripts/UpgradeStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UpgradeStats : MonoBehaviour, IPointerDownHandler
+{
+
+    public string upgradeName;
+    public string upgradeDescription;
+    public int upgradePrice;
+    
+    Text upgradeNameText;
+    Text upgradeDescriptionText;
+    Text upgradePriceText;
+
+    int upgradeLevel;
+
+    void Start()
+    {
+        upgradeNameText = GameObject.Find("UpgradeName").GetComponent<Text>();
+        upgradeDescriptionText = GameObject.Find("UpgradeDescription").GetComponent<Text>();
+        upgradePriceText = GameObject.Find("UpgradeCost").GetComponent<Text>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        upgradeNameText.text = upgradeName;
+        upgradeDescriptionText.text = upgradeDescription;
+        upgradePriceText.text = "LEVEL " + upgradeLevel + " AVAILABLE FOR      " + upgradePrice;
+    }
+}
