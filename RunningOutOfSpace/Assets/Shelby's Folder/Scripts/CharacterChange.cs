b6452a6194577df5e54b9ac85a96d185e9950a8f
Shelby Juno <shelbyjuno@gmail.com>
2018-08-12 17:47:26 -0500

    UI
    -Added fade to main menu
    -Added fade to character menu
    -Code tweaks
    -Added new UI
    -Added a dontdestroy manager with player name and sprite ID.
    -Comments

---
+++ RunningOutOfSpace/Assets/Shelby's Folder/Scripts/CharacterChange.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterChange : MonoBehaviour {
+
+    public Sprite[] characterSprites;
+
+    string playerName;
+    int characterSpriteID = 0;
+
+    Image characterSprite;
+
+    void Start()
+    {
+        characterSprite = GameObject.Find("CharacterSprite").GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        playerName = GameObject.Find("NameEnter").GetComponent<InputField>().text;
+    }
+
+    public string returnName()
+    {
+        return playerName;
+    }
+
+    public int returnSpriteID()
+    {
+        return characterSpriteID;
+    }
+
+    public void NextCharacter()
+    {
+        if (characterSpriteID == characterSprites.Length - 1)
+            characterSpriteID = 0;
+        else
+            characterSpriteID++;
+
+        characterSprite.sprite = characterSprites[characterSpriteID];
+    }
+    public void PreviousCharacter()
+    {
+        if (characterSpriteID == 0)
+            characterSpriteID = characterSprites.Length - 1;
+        else
+            characterSpriteID--;
+
+        characterSprite.sprite = characterSprites[characterSpriteID];
+    }
+}
