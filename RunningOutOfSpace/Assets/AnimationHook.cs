3a58a3ceb84e8134eff937916ddb783189728487
Sahilpohare <Sahilpohare@gmail.com>
2018-08-12 23:33:52 +0530

    Hey I have stuff working

---
+++ RunningOutOfSpace/Assets/AnimationHook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AnimationHook : MonoBehaviour {
+    public GameObject graphic;
+    public Animator anim;
+    public Enemy ai;
+    public Vector3 baseScale;
+    private void Start()
+    {
+        ai = GetComponent<Enemy>();
+        graphic = transform.GetChild(0).gameObject;
+        anim = graphic.GetComponent<Animator>();
+        baseScale = graphic.transform.localScale;
+    }
+    private void Update()
+    {
+        Vector3 vel = ai.aiAgent.velocity;
+        anim.SetFloat("Velocity", vel.magnitude);
+        if (ai.enemyStates == Enemy.EnemyStates.follow)
+        {
+            if (vel.x > 0)
+            {
+                graphic.transform.localScale = baseScale;
+            }
+            if (vel.x < 0)
+            {
+                graphic.transform.localScale = new Vector3(baseScale.x * -1,baseScale.y,baseScale.z);
+            }
+        }
+    }
+}
