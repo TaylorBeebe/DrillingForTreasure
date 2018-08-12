7e65a3cadb33d71913e4950b194c2e0a537c9776
Taylor <31827475+tdb1@users.noreply.github.com>
2018-08-11 20:37:47 -0400

    Updated Enemy Spawner Script. Not currently operational

--- RunningOutOfSpace/Assets/Scripts/SceneManager/EnemySpawner.cs
+++ RunningOutOfSpace/Assets/Scripts/SceneManager/EnemySpawner.cs
@@ -2,53 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour {
-    
+public class EnemySpawner : MonoBehaviour
+{
+
     //Array holding the different enemy types
     [SerializeField] GameObject Enemy1, Enemy2, Enemy3;
 
     //Get and create set for array so it can be initialized at start
-    public GameObject[] Enemyarray {
+    public GameObject[] Enemyarray
+    {
         get;
         private set;
     }
 
     //Current state of spawning
-    public enum SpawnState {
+    public enum SpawnState
+    {
         SPAWNING,
         WAITING,
         COUNTING
     };
-    
+
     /*Wave class currently elementary
     Wave class will eventually be used to calculate random waves based
     on level number
     */
     [System.Serializable]
-    public class Wave {
-        public string name;
-        public int count;
-        public float rate = 2f;
-        public Transform location;   
+    protected class Wave
+    {
+        private Stack<string> waveMakeup;
+        private int count;
+        private Vector2 spawnLocation;
+        private int rate = 2;
+        private bool doesGathererSpawn;
+
+        public Stack<string> GetWave()
+        {
+            return waveMakeup;
+        }
+        public void SetWave(Stack<string> wave)
+        {
+            waveMakeup = wave;
+        }
+        public int GetCount()
+        {
+            return count;
+        }
+        public void SetCount(int newCount)
+        {
+            count = newCount;
+        }
+        public Vector2 GetSpawnLocation()
+        {
+            return spawnLocation;
+        }
+        public void SetSpawnLocation(Vector2 newSpawnLocation)
+        {
+            spawnLocation = newSpawnLocation;
+        }
+        public int GetRate()
+        {
+            return rate;
+        }
+        public void SetRate(int newRate)
+        {
+            rate = newRate;
+        }
+        public bool GetDoesGathererSpawn()
+        {
+            //roll dice here. super small chance
+            return doesGathererSpawn;
+        }
     }
-
-    //Holds waves. Will need to be generated
-    public Wave[] waves;
-
-    //current index of wave
-    private int nextWave = 0;
 
     //wait time for checking if any enemies are alive
     public float delaySearchEnemies = 2f;
 
     //predetermined time between waves. Will eventually be a function of level
-    public float timeBetweenWaves = 5f;
+    private float timeBetweenWaves = 5f;
 
     //time until next wave spawn
     public float waveCountdown;
-
-    //bool holds state of level
-    public bool levelOver;
 
     //get main camera
     public Camera mainCamera;
@@ -59,17 +93,19 @@ public class EnemySpawner : MonoBehaviour {
     //holds aspect ration
     private float screenAspect;
 
-    //gets camera height/2
+    //holds camera height/2
     private float cameraHeight;
 
-    //gets camera width/2
+    //holds camera width/2
     private float cameraWidth;
+
 
     /* @ Param: None
      * @ Pre: None
      * @ Post: Initialize Variables
      */
-    void Start() {
+    void Start()
+    {
         //initialize the wave countdown max
         waveCountdown = timeBetweenWaves;
 
@@ -77,7 +113,7 @@ public class EnemySpawner : MonoBehaviour {
         Enemyarray = new GameObject[] {
             Enemy1, Enemy2, Enemy3
         };
-        
+
         screenAspect = (float)Screen.width / (float)Screen.height;
         cameraHeight = mainCamera.orthographicSize;
         cameraWidth = screenAspect * cameraHeight;
@@ -87,34 +123,22 @@ public class EnemySpawner : MonoBehaviour {
      * @ Pre: None
      * @ Post: Update run every frame
      */
-    void Update() {
+    void Update()
+    {
 
-        //delaySearchEnemies -= Time.deltaTime;
-
-        if (state == SpawnState.WAITING)
+        if (state == SpawnState.COUNTING)
         {
-            /*
-            //check if enemies are alive?
-            if (delaySearchEnemies <= 0) {
-                if (AreEnemiesAlive())
-                {
-                    //check if there are more enemies to spawn?
-                }
-                delaySearchEnemies = 2f;
-            }
-            */
-        }
-        else if (state == SpawnState.COUNTING) {
             waveCountdown -= Time.deltaTime;
         }
-        if (waveCountdown <= 0) {
+        if (waveCountdown <= 0)
+        {
             //If we are already spawning, we don't want to start a new coroutine for spawning
             if (state != SpawnState.SPAWNING)
             {
+                Wave newWave = new Wave();
                 Debug.Log("State != Spawning");
                 //Coroutine spawns waves so they don't need to be spawned all at one time
-                StartCoroutine(SpawnWave(waves[nextWave]));
-                nextWave += 1;
+                StartCoroutine(SpawnWave(newWave));
             }
         }
     }
@@ -123,31 +147,25 @@ public class EnemySpawner : MonoBehaviour {
      * @ Pre: Enemies not currently spawning
      * @ Post: Enemies in wave spawned
      */
-    IEnumerator SpawnWave(Wave wave) {
-        
+    IEnumerator SpawnWave(Wave wave)
+    {
+
         //start spawning waves. Set state so more waves aren't spawned simultaneously
         //this will be replaced with wave spawning counter, allowing waves to spawn concurrently
         //as a function of level
         state = SpawnState.SPAWNING;
 
         //increment through elements in wave
-        for (int i = 0; i < wave.count; i++) {
+        for (int i = 0; i < wave.GetCount(); i++)
+        {
             Vector2 spawnLocation = GetRandomSpawnLocation();
             SpawnEnemy(spawnLocation);
-            yield return new WaitForSeconds(1f / wave.rate);
+            yield return new WaitForSeconds(1f / wave.GetRate());
         }
-        Debug.Log("wave length: " + waves.Length);
-        //check if we are done spawning waves
-        if (nextWave == waves.Length)
-        {
-            state = SpawnState.WAITING;
-            Debug.Log("SpawnState = Waiting");
-        }
-        //if not, set state to counting
-        else {
-            state = SpawnState.COUNTING;
-            Debug.Log("SpawnState = Counting");
-        }
+
+        state = SpawnState.COUNTING;
+
+        Debug.Log("SpawnState = Counting");
 
         //reset wave countdown
         waveCountdown = timeBetweenWaves;
@@ -155,13 +173,19 @@ public class EnemySpawner : MonoBehaviour {
         yield break;
     }
 
+
     /* @ Param: Spawn location enemy will be spawned at
      * @ Pre: None
      * @ Post: Enemy spawned at location
      */
-    void SpawnEnemy(Vector2 spawnLocation) {
-        Debug.Log("Spawning Enemies at " + spawnLocation);
-        Instantiate(Enemy1, spawnLocation, Quaternion.identity);
+    void SpawnEnemy(Vector2 spawnLocation)
+    {
+
+        //Debug.Log("Spawning Enemies at " + spawnLocation);
+        //Pick an enemy to spawn randomly
+        int enemyToSpawn = Random.Range(0, 3);
+        //Instantiate the enemy at the spawn location passed in
+        Instantiate(Enemyarray[enemyToSpawn], spawnLocation, Quaternion.identity);
     }
 
     /* @ Param: None
@@ -183,24 +207,44 @@ public class EnemySpawner : MonoBehaviour {
      * @ Pre: Enemies are spawning
      * @ Post: Returns vector2 location where enemy(s) will spawn
      */
-    Vector2 GetRandomSpawnLocation() {
+    Vector2 GetRandomSpawnLocation()
+    {
 
+        //Either the width or the height values need to be maxed to keep the spawning outside screen view
         int whichNumberToMax = Random.Range(0, 2);
+        int[] randomizeMaxMin = { -1, 1 };
+        int randomizedValue = randomizeMaxMin[Random.Range(0, 2)];
+        Vector2 returnValue;
 
+        //If it's zero, max out the camera width
         if (whichNumberToMax == 0)
         {
-            Debug.Log("returning value");
-            return new Vector2(mainCamera.transform.position.x + cameraWidth, mainCamera.transform.position.y + Random.Range(cameraHeight * -1, cameraHeight));
+            //Debug.Log("returning returning maxed width");
+            returnValue = new Vector2(((mainCamera.transform.position.x + cameraWidth) * randomizedValue) + randomizedValue, mainCamera.transform.position.y + Random.Range(cameraHeight * -1, cameraHeight));
+
         }
+        //otherwise, max out camera height value
         else
         {
-            Debug.Log("returning value");
-            return new Vector2(mainCamera.transform.position.x + Random.Range(cameraWidth * -1, cameraWidth), mainCamera.transform.position.y + cameraHeight);
+            //Debug.Log("returning maxed height");
+            returnValue = new Vector2(mainCamera.transform.position.x + Random.Range(cameraWidth * (-1), cameraWidth), ((mainCamera.transform.position.y + cameraHeight) * randomizedValue) + randomizedValue);
 
         }
-
-        //For Testing
-        //return new Vector2(mainCamera.transform.position.x + cameraWidth, mainCamera.transform.position.y + cameraHeight);
+        //Debug.Log("Returning: " + returnValue);
+        return returnValue;
     }
 
+    /* @ Param: Current level player is on
+     * @ Pre: Wave is being generated
+     * @ Post: Returns a stack of names of the enemies to be spawned
+     */
+    Stack<string> GenerateWaveMakeup(int count)
+    {
+
+        Stack<string> wave = new Stack<string>();
+
+
+
+        return wave;
+    }
 }
