47ace6226bdaecad0d480b34784dc1905cd4cb19
Sahilpohare <Sahilpohare@gmail.com>
2018-08-12 14:27:13 +0530

    revert

---
+++ RunningOutOfSpace/Assets/Scripts/EnemyAI/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class Enemy : MonoBehaviour {
+
+    public AIPath aiAgent;
+    public AIDestinationSetter AIDestination;
+    public Transform target;
+    public float health = 100f;
+
+    public enum EnemyStates {
+        attack,
+        follow,
+        dead
+    }
+    public EnemyStates enemyStates;
+    public virtual void OnCreate()
+    {
+        aiAgent = GetComponent<AIPath>();
+        AIDestination = GetComponent<AIDestinationSetter>();
+        EnemyManager.enemyManager.enemies.Add(this);
+
+    }
+    public virtual void Execute()
+    {
+
+    }
+    public virtual void OnDeath(float WaitBeforeDestroying)
+    {
+        Destroy(this, WaitBeforeDestroying);
+    }
+}
