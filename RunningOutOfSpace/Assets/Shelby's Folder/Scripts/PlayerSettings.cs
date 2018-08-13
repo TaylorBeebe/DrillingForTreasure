574f87b513b8967ad7d38cdd2b358bc61110417e
Cedric Claessens <cedricc2004@gmail.com>
2018-08-13 05:11:38 -0700

    More Main Menu Fixing

--- RunningOutOfSpace/Assets/Shelby's Folder/Scripts/PlayerSettings.cs
+++ RunningOutOfSpace/Assets/Shelby's Folder/Scripts/PlayerSettings.cs
@@ -8,6 +8,8 @@ public class PlayerSettings : MonoBehaviour {
     public string playerName;
     public int characterSpriteID = 0;
 
+    [SerializeField] int gameSceneBuildIndex;
+
 
     void Start()
     {
@@ -21,5 +23,6 @@ public class PlayerSettings : MonoBehaviour {
         var canv = GameObject.Find("Canvas").GetComponent<CharacterChange>();
         playerName = canv.returnName();
         characterSpriteID = canv.returnSpriteID();
+        SceneManager.LoadScene(gameSceneBuildIndex);
     }
 }
