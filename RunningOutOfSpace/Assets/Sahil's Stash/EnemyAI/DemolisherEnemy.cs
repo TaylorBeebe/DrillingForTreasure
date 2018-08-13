29f98154b947de6cd039758e4e373ff3e0448178
Taylor <31827475+tdb1@users.noreply.github.com>
2018-08-13 15:49:04 -0400

    Demolisher Working

--- RunningOutOfSpace/Assets/Sahil's Stash/EnemyAI/DemolisherEnemy.cs
+++ RunningOutOfSpace/Assets/Sahil's Stash/EnemyAI/DemolisherEnemy.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.Rendering;
 
 public class DemolisherEnemy : Enemy
 {
 
     public float attackAnimationTime = 1.2f;
+    bool wasHit = false;
+    SpriteRenderer renderer;
+    public Sprite deathSprite;
+    public Sprite attackSprite;
+    public Sprite aliveSprite;
+
+    SortingGroup group;
 
     public override void Start()
     {
         base.Start();
+        group = GetComponent<SortingGroup>();
+        renderer = this.gameObject.GetComponentInChildren<SpriteRenderer>();
         target = enemyManager.Drill;
     }
     public override void Update()
@@ -18,6 +28,17 @@ public class DemolisherEnemy : Enemy
         base.Update();
         aiAgent.canMove = canMove;
         //aiAgent.canMove = true;
+    }
+    public void LateUpdate()
+    {
+        if (wasHit)
+        {
+            wasHit = false;
+        }
+        else
+        {
+            renderer.color = Color.white;
+        }
     }
 
     public override void OnFollow()
@@ -29,7 +50,7 @@ public class DemolisherEnemy : Enemy
     {
         canMove = false;
         canAttack = false;
-
+        AttackAnimation();
         target.GetComponent<HealthAndVariables>().TakeDamage(damage);
         Invoke("UpdateCanAttack", attackAnimationTime);
     }
@@ -37,11 +58,33 @@ public class DemolisherEnemy : Enemy
     public override void OnDeath()
     {
         base.OnDeath();
+        renderer.sprite = deathSprite;
     }
 
     void UpdateCanAttack()
     {
         canAttack = true;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Bullet")
+        {
+            other.GetComponent<Bullet>().Destroy(other.transform.position);
+            GetComponent<HealthAndVariables>().TakeDamage(20);
+            renderer.color = Color.red;
+            wasHit = true;
+        }
+    }
+
+    void AttackAnimation()
+    {
+        renderer.sprite = attackSprite;
+        Invoke("NormalAnimation", 1f);
+    }
+
+    void NormalAnimation()
+    {
+        renderer.sprite = aliveSprite;
     }
 }
 
