65fe1d4a285cd704a0b2b84e2187c2ac59f90277
Taylor <31827475+tdb1@users.noreply.github.com>
2018-08-13 14:20:48 -0400

    New Merge Attempt

---
+++ RunningOutOfSpace/Assets/Sahil's Stash/EnemyAI/StickerEnemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class StickerEnemy : Enemy
+{
+
+    public GameObject headSticker;
+    public float attackAnimationTime = 1.2f;
+
+    public override void Start()
+    {
+        base.Start();
+        target = enemyManager.Player;
+    }
+    public override void Update()
+    {
+        base.Update();
+        aiAgent.canMove = canMove;
+        //aiAgent.canMove = true;
+    }
+
+    public override void OnFollow()
+    {
+        base.OnFollow();
+    }
+
+    public override void OnAttack()
+    {
+        base.OnAttack();
+        Instantiate(headSticker, target.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    public override void OnDeath()
+    {
+        base.OnDeath();
+    }
+
+    void UpdateCanAttack()
+    {
+        canAttack = true;
+    }
+}
+
