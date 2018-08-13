151f2aea189e842843a4b66d21ba40becf0cc558
Shelby Juno <shelbyjuno@gmail.com>
2018-08-12 23:51:44 -0500

    Adjusted UI to player
    -Title

---
+++ RunningOutOfSpace/Assets/Shelby's Folder/Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour {
+
+    public float maxHealth;
+    public float health;
+
+    public float maxDrillHealth;
+    public float drillHealth;
+
+    public float maxWeaponCharge;
+    public float weaponCharge;
+
+	void Update ()
+    {
+        GameObject.Find("PlayerHealthBar").GetComponent<Image>().fillAmount = health / maxHealth;
+        GameObject.Find("DrillHealthBar").GetComponent<Image>().fillAmount = drillHealth / maxDrillHealth;
+        GameObject.Find("WeaponEnergy").GetComponent<Image>().fillAmount = weaponCharge / maxWeaponCharge;
+    }
+}
