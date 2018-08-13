291659f9a95d1c49b3ead58042cc399379336804
Shelby Juno <shelbyjuno@gmail.com>
2018-08-12 20:21:41 -0500

    Scenes
    Added scenes to build

---
+++ RunningOutOfSpace/Assets/Shelby's Folder/Scripts/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class LoadScene : MonoBehaviour, IPointerDownHandler
+{
+    public string SceneToLoad;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("SceneToLoad");
+    }
+}
